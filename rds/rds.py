9259074f2004a1a25f64533939c71d72ce5a544f
Andrew Baumann <andrew.m.baumann@gmail.com>
2018-09-12 19:37:21 -0500

    Better pytest practices and starting to add logic.

--- rds/rds.py
+++ rds/rds.py
@@ -2,13 +2,66 @@
 """
 A utility to synchronize directories across a local network.
 """
-
+import hashlib
+import shutil
 
 def main():
     """
     Main entry to the application.
     """
     print("Hello World!")
+
+def _sync_trees(system_of_record, backup_system):
+    """
+    This method will compare the children of these two directories and then recursively call itself for their children.
+    :param system_of_record:
+    :param backup_system:
+    :return:
+    """
+    backup_system_children = list(backup_system.iterdir());
+    system_of_record_children = list(system_of_record.iterdir())
+    for sor_child in system_of_record_children:
+        found = False
+        for bs_child in backup_system_children:
+            if sor_child.is_file():
+                if _is_file_same(sor_child, bs_child):
+                    found = True
+                    break
+            else:
+                if _is_dir_same(sor_child, bs_child):
+                    found = True
+                    _sync_trees(sor_child, bs_child)
+        if not found:
+            if sor_child.is_file():
+                shutil.copy(str(sor_child), str(backup_system.absolute()) + "/" + sor_child.name)
+            else:
+                shutil.copytree(str(sor_child), str(backup_system.absolute()) + "/" + sor_child.name)
+
+
+
+
+
+def _is_file_same(left, right):
+    """
+    Must have the same name and the same hash
+    :param left:
+    :param right:
+    :return:
+    """
+    left_name = left.name
+    left_hash = _hash(left.absolute)
+    right_name = right.name
+    right_hash = _hash(right.absolute)
+    return left_name == right_name and left_hash == right_hash
+
+def _is_dir_same(left, right):
+    return left.name == right.name
+
+def _hash(filename):
+    with open(filename, 'rb') as file:
+        bytes = file.read()
+        readable_hash = hashlib.sha256(bytes).hexdigest()
+        return readable_hash
 
 
 if __name__ == '__main__':
--- tests/test_rds.py
+++ tests/test_rds.py
@@ -1,11 +1,41 @@
-import unittest
+import pytest
+import os
+import shutil
+from pathlib import Path
 from rds import rds
 
 
-class TestStringMethods(unittest.TestCase):
+class TestRdsMethods():
+
+    def setup_method(self, test_method):
+        Path('./tests/test_data/a').mkdir(parents=True, exist_ok=True)
+        Path('./tests/test_data/b').mkdir(parents=True, exist_ok=True)
+
+    def teardown_method(self, test_method):
+        shutil.rmtree(Path('./tests/test_data/'), True)
+
     def test_main(self):
         rds.main()
 
+    def test_sync_empty_directories(self):
+        rds._sync_trees(Path('./tests/test_data/a'), Path('./tests/test_data/b'))
+        assert Path('./tests/test_data/a').is_dir()
+        assert Path('./tests/test_data/a').exists()
+        assert Path('./tests/test_data/b').is_dir()
+        assert Path('./tests/test_data/b').exists()
+        assert 0 == len(list(Path('./tests/test_data/a').iterdir()))
+        assert 0 == len(list(Path('./tests/test_data/b').iterdir()))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_sync_trees_file_exists_on_a_but_not_b(self):
+        child1 = Path('./tests/test_data/a/childA')
+        child1.touch()
+        rds._sync_trees(Path('./tests/test_data/a'), Path('./tests/test_data/b'))
+        assert 1 == len(list(Path('./tests/test_data/a').iterdir()))
+        assert 1 == len(list(Path('./tests/test_data/b').iterdir()))
+
+    def test_sync_trees_file_exists_on_a_and_b(self):
+        pass
+
+    def test_sync_trees_file_exists_on_b_but_not_a(self):
+        pass
+
